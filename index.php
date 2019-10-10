2fc3f4118f9a63627796dfa6de0bf79c04401f17
Brandon Hughes <whughes511@gmail.com>
2019-10-10 13:54:30 -0500

    Testing authentication (part 1)

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+if(!$_SESSION['broadwn6_logins'])
+{
+    header('location:login.php');
+}
+?>
+
+<h1>Welcome! You are authenticated.....!</h1>
+
+<a href="logout.php">Logout!</a>
